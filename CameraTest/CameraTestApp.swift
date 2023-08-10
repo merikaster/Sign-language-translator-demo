db83ebeed73b2fcac54fd286908df1c361548cb1
Rakan <charmerrk@gmail.com>
2023-08-10 17:02:28 +0800

    Initial Commit

---
+++ CameraTest/CameraTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraTestApp.swift
+//  CameraTest
+//
+//  Created by Rakan on 2023/8/10.
+//
+
+import SwiftUI
+
+@main
+struct CameraTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
